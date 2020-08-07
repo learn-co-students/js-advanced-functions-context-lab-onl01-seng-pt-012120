9aca1255f215ea0daccd1f2bd1e204cd79b31c13
haileybongo <haileyebongo@gmail.com>
2020-08-07 10:55:46 -0700

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,82 @@ let allWagesFor = function () {
 
     return payable
 }
+
+    function createEmployeeRecord(worker){
+        let newWorker = []
+        newWorker.firstName = worker[0]
+        newWorker.familyName = worker[1]
+        newWorker.title = worker[2]
+        newWorker.payPerHour = worker[3]
+        newWorker.timeInEvents = []
+        newWorker.timeOutEvents = []
+        return newWorker
+   
+    }
+
+    function createEmployeeRecords(workers){
+        let workerList = []
+        for (const array of workers){
+            let newWorker = createEmployeeRecord(array)
+            workerList.push(newWorker)
+        }
+        return workerList
+    }
+   
+    function createTimeInEvent(timeIn){
+
+        let splitTime = timeIn.split(" ")
+        this.timeInEvents.push({
+            type: "TimeIn",
+            date: splitTime[0],
+            hour: parseInt(splitTime[1])
+        }
+   
+        )
+        return this
+    }
+   
+
+ function createTimeOutEvent(timeOut){
+    let splitTime = timeOut.split(" ")
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: splitTime[0],
+        hour: parseInt(splitTime[1])
+    }
+
+    )
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let inEvent = this.timeInEvents.find(function(punch){
+        return punch.date === date
+    })
+    let outEvent = this.timeOutEvents.find(function(punch){
+        return punch.date === date
+    })
+    let hoursWorked = (outEvent.hour - inEvent.hour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(date){
+    let hours = hoursWorkedOnDate.call(this, date)
+    return hours * this.payPerHour
+}
+
+function calculatePayroll(employees){
+    let employeeWages = employees.map(function(employee){
+        return allWagesFor.call(employee)
+    })
+
+    return employeeWages.reduce(function(total, i){
+        return total +i
+    })
+}
+
+
+function findEmployeeByFirstName(employees, name){
+    return employees.find(function(e){ return e.firstName === name})
+
+}
