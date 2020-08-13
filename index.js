fdd16c012697c4f07bd63f1a24466dfb8e809bf8
d-otis <dan@dan-foley.com>
2020-08-12 20:01:40 -0400

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ let allWagesFor = function () {
 }
 
 const createEmployeeRecord = function(arr) {
+  let a = 'closure?'
   return {
     firstName: arr[0],
     familyName: arr[1],
@@ -39,10 +40,11 @@ const createEmployeeRecords = function(arr) {
   // PROCESSING:
   // For each element of outer array => create an employee
   
-  return arr.map(createEmployeeRecord)
+  
 
   // RETURNS:
   // Array of Employee Objects 
+  return arr.map(createEmployeeRecord)
 }
 
 const createTimeInEvent = function(timestamp) {
@@ -108,52 +110,3 @@ const calculatePayroll = function(emps) {
   // Pay Owed For All Dates (one amount)
   return wages.reduce((e, total) => e + total)
 }
-
-const csvDataEmployees = [
-  ["Thor", "Odinsson", "Electrical Engineer", 45],
-  ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
-  ["Natalia", "Romanov", "CEO", 150],
-  ["Darcey", "Lewis", "Intern", 15],
-  ["Jarvis", "Stark", "CIO", 125],
-  ["Anthony", "Stark", "Angel Investor", 300]
-]
-
-const csvTimesIn = [
-  ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
-  ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
-  ["Natalia", ["2018-01-03 1700", "2018-01-05 1800", "2018-01-03 1300"]],
-  ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
-  ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
-  ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
-]
-
-const csvTimesOut = [
-  ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
-  ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
-  ["Natalia", ["2018-01-03 2300", "2018-01-05 2300", "2018-01-03 2300"]],
-  ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
-  ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
-  ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
-]
-
-let employeeRecords = createEmployeeRecords(csvDataEmployees)
-
-employeeRecords.forEach(function (rec) {
-  let timesInRecordRow = csvTimesIn.find(function (row) {
-    return rec.firstName === row[0]
-  })
-
-  let timesOutRecordRow = csvTimesOut.find(function (row) {
-    return rec.firstName === row[0]
-  })
-
-  timesInRecordRow[1].forEach(function(timeInStamp){
-    createTimeInEvent.call(rec, timeInStamp)
-  })
-
-  timesOutRecordRow[1].forEach(function(timeOutStamp){
-    createTimeOutEvent.call(rec, timeOutStamp)
-  })
-}) 
-
-calculatePayroll(employeeRecords)
